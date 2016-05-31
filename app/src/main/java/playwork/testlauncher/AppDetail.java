0c7e84f09dd16f4dd83d784dea31f1d4458dc5ab
Petch <silverpetch@gmail.com>
2016-05-31 17:29:31 +0700

    Create

---
+++ app/src/main/java/playwork/testlauncher/AppDetail.java
@@ -0,0 +1,12 @@
+package playwork.testlauncher;
+
+/**
+ * Created by mac on 5/30/16 AD.
+ */
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
